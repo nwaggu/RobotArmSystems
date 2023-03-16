d23512995781c98e20b2cf600a17cf07b92d07da
nnamd <119299028+nwaggu@users.noreply.github.com>
2023-03-16 02:38:42 -0700

    Update CameraSensing.py

--- ArmPi/Functions/CameraSensing.py
+++ ArmPi/Functions/CameraSensing.py
@@ -19,6 +19,7 @@ class ColorSensing():
     def __init__(self):
         self.count = 0
         self.center_list = []
+        self.start_count_t1 = True
         #Camera setup
         self.my_camera = Camera.Camera()
         self.my_camera.camera_open()
@@ -110,19 +111,19 @@ class ColorSensing():
         if distance < 0.5:
             self.count += 1
             self.center_list.extend((self.world_x, self.world_y))
-            if start_count_t1:
-                start_count_t1 = False
+            if self.start_count_t1:
+                self.start_count_t1 = False
                 t1 = time.time()
             if time.time() - t1 > 1:
                 self.rotation_angle = self.rect[2] 
-                start_count_t1 = True
+                self.start_count_t1 = True
                 self.world_X, self.world_Y = np.mean(np.array(self.center_list).reshape(count, 2), axis=0)
                 self.center_list = []
                 self.count = 0
                 self.start_pick_up = True
         else:
             t1 = time.time()
-            start_count_t1 = True
+            self.start_count_t1 = True
             self.center_list = []
             self.count = 0
 
