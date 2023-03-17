8a247464190ef8b560a6214bfcf3267dfa7def61
nnamd <119299028+nwaggu@users.noreply.github.com>
2023-03-16 22:51:41 -0700

    Update CameraSensing.py

--- ArmPi/Functions/CameraSensing.py
+++ ArmPi/Functions/CameraSensing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # coding=utf8
-import ArmMove as arm
-from concurrency import Bus
+
+
 import sys
 sys.path.append('/home/pi/ArmPi/')
 import cv2
@@ -15,6 +15,22 @@ import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 import atexit
 
+import concurrent.futures
+from readerwriterlock import rwlock
+
+class Bus():
+    def __init__(self, message):
+        self.message = message
+        self.lock = rwlock.RWLockWriteD()
+    
+    def write(self, new_message):
+        with self.lock.gen_wlock():
+            self.message = new_message
+        
+    def read(self):
+        with self.lock.gen_rlock():
+            message = self.message
+        return message
 
 
 
